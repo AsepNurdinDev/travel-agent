00084d687c086d84f09bf7ac768699bd3bf8c24e
AsepNurdinDev <asep@student.undiksha.ac.id>
2026-08-27 16:40:25 +0800

    update bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
                 Request::HEADER_X_FORWARDED_PORT |
                 Request::HEADER_X_FORWARDED_PROTO,
         );
+
+        $middleware->validateCsrfTokens(except: [
+            'midtrans/notification',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
